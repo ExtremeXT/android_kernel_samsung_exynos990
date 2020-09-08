72037bf1298bc6d841673d7886be26a868f6a688
Denis Efremov <efremov@linux.com>
2020-09-09 01:52:11 +0300

    power: wl_blocker: drop redundant global initializers
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -36,7 +36,7 @@ suspend_state_t pm_suspend_target_state;
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 #include "boeffla_wl_blocker.h"
 
-char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
+char list_wl_search[LENGTH_LIST_WL_SEARCH];
 bool wl_blocker_active = false;
 bool wl_blocker_debug = false;
 
