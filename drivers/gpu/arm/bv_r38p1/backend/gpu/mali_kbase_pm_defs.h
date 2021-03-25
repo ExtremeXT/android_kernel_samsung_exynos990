68e7acec0d626c26828a1c60406b67694965b5b2
Sidath Senanayake <sidaths@google.com>
2021-03-25 22:55:23 +0000

    mali_kbase: pm: Add adaptive power policy
    Adds a new power policy 'adaptive' based off 'coarse_demand'.
    The main difference is that while coarse_demand has a static
    power off hysteresis (i.e. the amount of time after the GPU
    goes idle that the platform idle callback is made), adaptive
    scales this up and down depending on the workload.
    
    If the GPU is frequently made active during the power off
    hysteresis period, then adaptive behaves identically to
    coarse_demand.
    
    If the GPU remains idle for DEFAULT_PM_ADAPTIVE_HYSTERESIS
    consecutive cycles, however, then the hysteresis period is
    reduced or disabled entirely.
    
    Bug: 183700775
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    Change-Id: I5619132b3e813a5b2a9420b0ff193f8aa586194a

--- drivers/gpu/arm/bv_r38p1/backend/gpu/mali_kbase_pm_defs.h
+++ drivers/gpu/arm/bv_r38p1/backend/gpu/mali_kbase_pm_defs.h
@@ -28,6 +28,7 @@
 
 #include "mali_kbase_pm_always_on.h"
 #include "mali_kbase_pm_coarse_demand.h"
+#include "mali_kbase_pm_adaptive.h"
 
 #if defined(CONFIG_PM_RUNTIME) || defined(CONFIG_PM)
 #define KBASE_PM_RUNTIME 1
@@ -210,6 +211,7 @@ struct kbasep_pm_tick_timer_state {
 union kbase_pm_policy_data {
 	struct kbasep_pm_policy_always_on always_on;
 	struct kbasep_pm_policy_coarse_demand coarse_demand;
+	struct kbasep_pm_policy_adaptive adaptive;
 };
 
 /**
@@ -509,6 +511,7 @@ struct kbase_pm_backend_data {
 				  CSF_DYNAMIC_PM_SCHED_IGNORE_IDLE | \
 				  CSF_DYNAMIC_PM_SCHED_NO_SUSPEND)
 #define COARSE_ON_DEMAND_PM_SCHED_FLAGS (0)
+#define ADAPTIVE_PM_SCHED_FLAGS (0)
 #if !MALI_CUSTOMER_RELEASE
 #define ALWAYS_ON_DEMAND_PM_SCHED_FLAGS (CSF_DYNAMIC_PM_SCHED_IGNORE_IDLE)
 #endif
@@ -520,6 +523,7 @@ enum kbase_pm_policy_id {
 #if !MALI_CUSTOMER_RELEASE
 	KBASE_PM_POLICY_ID_ALWAYS_ON_DEMAND,
 #endif
+	KBASE_PM_POLICY_ID_ADAPTIVE,
 	KBASE_PM_POLICY_ID_ALWAYS_ON
 };
 
