e718f3b32d496f7dd01d8c79fd85a19d002f110e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-02-26 00:52:53 -0600

    sched: psi: Fix compilaton without kperfmon
    Change-Id: I7b8feeb38c231bb129e92c7bf2c973cc6723be65
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -140,7 +140,6 @@
 #include <linux/file.h>
 #include <linux/poll.h>
 #include <linux/psi.h>
-#include <linux/ologk.h>
 #include "sched.h"
 
 static int psi_bug __read_mostly;
@@ -169,14 +168,6 @@ __setup("psi=", setup_psi);
 #define WINDOW_MIN_US 500000	/* Min window size is 500ms */
 #define WINDOW_MAX_US 10000000	/* Max window size is 10s */
 #define UPDATES_PER_WINDOW 10	/* 10 updates per window */
-
-#define MONITOR_WINDOW_MIN_NS 1000000000 /* 1s */
-#define MONITOR_THRESHOLD_MIN_NS 100000000 /* 100ms */
-
-#define PERFLOG_PSI_THRESHOLD	250
-#define AVG10			0
-#define AVG60			1
-#define AVG300			2
 
 static int lmkd_count;
 static int lmkd_cricount;
@@ -419,29 +410,6 @@ static u64 update_averages(struct psi_group *group, u64 now)
 			sample = period;
 		group->avg_total[s] += sample;
 		calc_avgs(group->avg[s], missed_periods, sample, period);
-		if (s % 2 && (LOAD_INT(group->avg[s][AVG10]) * 100 + LOAD_FRAC(group->avg[s][AVG10])) >= PERFLOG_PSI_THRESHOLD) {
-			u64 total_full = 0, total_some = 0;
-			int some, full;
-			char title[NR_PSI_RESOURCES][4] = {"IO", "MEM", "CPU"};
-			char *strtitle;
-
-			strtitle = title[s / 2];
-			some = s - 1;
-			full = s;
-
-			total_some = div_u64(group->total[PSI_AVGS][some], NSEC_PER_USEC);
-			total_full = div_u64(group->total[PSI_AVGS][full], NSEC_PER_USEC);
-
-			perflog(PERFLOG_UNKNOWN, "[PSI][%s][%s] avg10=[%lu.%02lu/%lu.%02lu]  avg60=[%lu.%02lu/%lu.%02lu]  avg300=[%lu.%02lu/%lu.%02lu] total=[%llu/%llu]",
-				   strtitle, (group == &psi_system) ? "SYSTEM" : "CGROUP",
-				   LOAD_INT(group->avg[some][AVG10]), LOAD_FRAC(group->avg[some][AVG10]),
-				   LOAD_INT(group->avg[full][AVG10]), LOAD_FRAC(group->avg[full][AVG10]),
-				   LOAD_INT(group->avg[some][AVG60]), LOAD_FRAC(group->avg[some][AVG60]),
-				   LOAD_INT(group->avg[full][AVG60]), LOAD_FRAC(group->avg[full][AVG60]),
-				   LOAD_INT(group->avg[some][AVG300]), LOAD_FRAC(group->avg[some][AVG300]),
-				   LOAD_INT(group->avg[full][AVG300]), LOAD_FRAC(group->avg[full][AVG300]),
-				   total_some, total_full);
-		}
 	}
 
 	return avg_next_update;
@@ -568,25 +536,12 @@ static u64 update_triggers(struct psi_group *group, u64 now)
 		/* Calculate growth since last update */
 		growth = window_update(&t->win, now, total[t->state]);
 
-		if ((t->win.size >= MONITOR_WINDOW_MIN_NS)){
-			if (t->state==3) { //FULL
-				if(growth < 5000000000 && growth > psi_full_max) {
-					psi_full_max = growth;
-				}
-			}
-		}
-
 		if (growth < t->threshold)
 			continue;
 
 		/* Limit event signaling to once per window */
 		if (now < t->last_event_time + t->win.size)
 			continue;
-
-		if ((t->win.size >= MONITOR_WINDOW_MIN_NS) && 
-		    (t->threshold >= MONITOR_THRESHOLD_MIN_NS))
-			printk_deferred("psi: %s %llu %llu %d %llu %llu\n", __func__, now,
-			       t->last_event_time, t->state, t->threshold, growth);
 
 		/* Generate an event */
 		if (cmpxchg(&t->event, 0, 1) == 0)
