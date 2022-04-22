7023ae2f7ca21d83e33f0e07c28fefe30cedb22a
Kalesh Singh <kaleshsingh@google.com>
2022-04-22 08:47:55 -0700

    ANDROID: Make MGLRU aware of speculative faults
    Bug: 228525049
    Bug: 227651406
    Change-Id: Ib3d45f74c5c23cbb1af7aaf95c90826baf406c7a
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4431,6 +4431,9 @@ static vm_fault_t __handle_mm_fault(struct vm_area_struct *vma,
 	return handle_pte_fault(&vmf);
 }
 
+static void lru_gen_enter_fault(struct vm_area_struct *vma);
+static void lru_gen_exit_fault(void);
+
 #ifdef CONFIG_SPECULATIVE_PAGE_FAULT
 /*
  * Tries to handle the page fault in a speculative way, without grabbing the
@@ -4628,7 +4631,9 @@ int __handle_speculative_fault(struct mm_struct *mm, unsigned long address,
 	}
 
 	mem_cgroup_enter_user_fault();
+	lru_gen_enter_fault(vmf.vma);
 	ret = handle_pte_fault(&vmf);
+	lru_gen_exit_fault();
 	mem_cgroup_exit_user_fault();
 
 	/*
