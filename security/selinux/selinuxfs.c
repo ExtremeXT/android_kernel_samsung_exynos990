04e2d0107141a4191ddad243140878556ba36112
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-05-29 12:33:42 +0300

    selinux: Add togglable selinux boot status

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -160,6 +160,14 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	new_value = !!new_value;
 	old_value = enforcing_enabled(state);
 
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+	// If always enforce option is set, selinux is always enforcing
+	new_value = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+	// If always permissive option is set, selinux is always permissive
+	new_value = 0;
+#endif
+
 	if (new_value != old_value) {
 		length = avc_has_perm(&selinux_state,
 				      current_sid(), SECINITSID_SECURITY,
